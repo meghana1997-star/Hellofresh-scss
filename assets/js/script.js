8d7d83cce0f355049128519883e77d447b881ad7
meghana1997-star <79625389+meghana1997-star@users.noreply.github.com>
2021-09-22 09:38:54 +0530

    js added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,42 @@
+// slider
 
+var slider = document.querySelector('.slider');
 
+if (slider) {
+    $('.slider').slick({
+        dots: true,
+        autoplay: true,
+        arrows: true,
 
+        slidesToShow: 3,
+        slidesToScroll: 1
+    });
+}
 
+// lightbox
 
+var box = document.querySelectorAll('.content-box');
+for (var i = 0; i < box.length; i++) {
+    box[i].addEventListener('click', function (e) {
+        var boxElem = e.target.getAttribute("src");
+        lightBox(boxElem);
+    })
+}
+
+var ligBox = document.querySelector('.box');
+var elem = document.querySelector('.image-box');
+
+function lightBox(box) {
+    ligBox.classList.add('active');
+    elem.setAttribute("src", box);
+}
+
+ligBox.addEventListener('click', function (e) {
+    var close = document.querySelector('.close');
+    if (ligBox == e.target || close == e.target) {
+        ligBox.classList.remove('active');
+    }
+})
 
 
 
